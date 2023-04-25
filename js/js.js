219ddf655f9521e5cd4b4ab71c0f03c74730070a
Mdaytharethan <46084818+Mdaytharethan@users.noreply.github.com>
2023-04-25 02:12:57 +0000

    input test

---
+++ js/js.js
@@ -0,0 +1,9 @@
+let x = 15 
+console.log(x);
+function main(){
+    if(document.getElementById.text_1!=String){
+        console.log("error");
+        window.alert("error"+100);
+        window.alert(Date());
+    }
+}
